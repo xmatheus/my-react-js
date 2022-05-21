a0cee370d0d0fb3620c7b4fc015fadd585dee44b
xmatheus <matheuscorreia559@gmail.com>
2022-05-21 15:56:27 -0300

    refactor: split code

---
+++ src/main-page.js
@@ -0,0 +1,13 @@
+import React from "../lib/my-react";
+import "./index.css";
+import "./App.css";
+
+export default function MyPage() {
+  return (
+    <div>
+      <p className="App" id="one">
+        My Reactjs app
+      </p>
+    </div>
+  );
+}
